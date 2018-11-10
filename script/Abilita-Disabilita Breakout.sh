02dc0a7e3f35de3ec7578a7f1ba1e4264d74a1b5
vince87 <vincenzo.bini@gmail.com>
2018-11-11 00:33:32 +0100

    Create Abilita-Disabilita Breakout.sh

---
+++ script/Abilita-Disabilita Breakout.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+sudo grep '#mk_arcade_joystick_rpi' /etc/modules > /dev/null 2>&1
+if [ $? -eq 0 ] ; then
+
+echo "La breakout non è attiva!"
+sleep 4
+echo "Attivo la breakout!"
+
+sudo perl -p -i -e 's/#mk_arcade_joystick_rpi/mk_arcade_joystick_rpi/g' /etc/modules
+
+sudo perl -p -i -e 's/#dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/g' /boot/config.txt
+
+sudo modprobe mk_arcade_joystick_rpi map=1,2
+
+echo "Modifiche effettuate!"
+
+else
+
+echo "La breakout è attiva!"
+sleep 4
+echo "Disattivo la breakout!"
+
+sudo perl -p -i -e 's/mk_arcade_joystick_rpi/#mk_arcade_joystick_rpi/g' /etc/modules
+
+sudo perl -p -i -e 's/dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/#dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/g' /boot/config.txt
+
+sudo modprobe -r mk_arcade_joystick_rpi
+
+echo "Modifiche effettuate!"
+
+fi
+
+sleep 5
