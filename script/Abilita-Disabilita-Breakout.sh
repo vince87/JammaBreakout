a5ae58104c6d73a820675c4d3f2d71453ee0af55
vince87 <vincenzo.bini@gmail.com>
2019-05-17 21:35:14 +0200

    Update Abilita-Disabilita-Breakout.sh

--- script/Abilita-Disabilita-Breakout.sh
+++ script/Abilita-Disabilita-Breakout.sh
@@ -9,6 +9,8 @@ echo "Attivo la breakout!"
 sudo perl -p -i -e 's/#mk_arcade_joystick_rpi/mk_arcade_joystick_rpi/g' /etc/modules
 
 sudo perl -p -i -e 's/#dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/g' /boot/config.txt
+sudo perl -p -i -e 's/#disable_audio_dither=1/disable_audio_dither=1/g' /boot/config.txt
+
 
 sudo modprobe mk_arcade_joystick_rpi map=1,2
 
@@ -23,6 +25,8 @@ echo "Disattivo la breakout!"
 sudo perl -p -i -e 's/mk_arcade_joystick_rpi/#mk_arcade_joystick_rpi/g' /etc/modules
 
 sudo perl -p -i -e 's/dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/#dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/g' /boot/config.txt
+sudo perl -p -i -e 's/disable_audio_dither=1/#disable_audio_dither=1/g' /boot/config.txt
+
 
 sudo modprobe -r mk_arcade_joystick_rpi
 
