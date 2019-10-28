8961d0762254c3e04926c7018f937c5f053057d0
vince87 <vincenzo.bini@gmail.com>
2019-10-28 17:31:57 +0100

    Update Abilita-Disabilita-Breakout.sh

--- script/Abilita-Disabilita-Breakout.sh
+++ script/Abilita-Disabilita-Breakout.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
-sudo grep '#mk_arcade_joystick_rpi' /etc/modules > /dev/null 2>&1
-if [ $? -eq 0 ] ; then
 
-echo "La breakout non è attiva!"
-sleep 4
-echo "Attivo la breakout!"
+HEIGHT=10
+WIDTH=60
+BACKTITLE="Jamma Breakout Menù"
+TITLE="Abilita/Disabilita Jamma Breakout"
+MENU="Scegliere un opzione:"
 
-sudo perl -p -i -e 's/#mk_arcade_joystick_rpi/mk_arcade_joystick_rpi/g' /etc/modules
-sudo perl -p -i -e 's/#dtoverlay=audremap,pins_18_19/dtoverlay=audremap,pins_18_19/g' /boot/config.txt
-sudo perl -p -i -e 's/#dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/g' /boot/config.txt
-sudo perl -p -i -e 's/#disable_audio_dither=1/disable_audio_dither=1/g' /boot/config.txt
+OPTIONS=(1 "Abilita Jamma Breakout"
+         2 "Disabilita Jamma Breakout")
 
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
 
-sudo modprobe mk_arcade_joystick_rpi map=1,2
-
-echo "Modifiche effettuate!"
-
-else
-
-echo "La breakout è attiva!"
-sleep 4
-echo "Disattivo la breakout!"
-
-sudo perl -p -i -e 's/mk_arcade_joystick_rpi/#mk_arcade_joystick_rpi/g' /etc/modules
-sudo perl -p -i -e 's/dtoverlay=audremap,pins_18_19/#dtoverlay=audremap,pins_18_19/g' /boot/config.txt
-sudo perl -p -i -e 's/dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/#dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/g' /boot/config.txt
-sudo perl -p -i -e 's/disable_audio_dither=1/#disable_audio_dither=1/g' /boot/config.txt
-
-
-sudo modprobe -r mk_arcade_joystick_rpi
-
-echo "Modifiche effettuate!"
-
-fi
-
+clear
+case $CHOICE in
+        1)
+        echo "Attivo la breakout!"
+        sudo perl -p -i -e 's/#mk_arcade_joystick_rpi/mk_arcade_joystick_rpi/g' /etc/modules
+        sudo perl -p -i -e 's/#dtoverlay=audremap,pins_18_19/dtoverlay=audremap,pins_18_19/g' /boot/config.txt
+        sudo perl -p -i -e 's/#dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/g' /boot/config.txt
+        sudo perl -p -i -e 's/#disable_audio_dither=1/disable_audio_dither=1/g' /boot/config.txt
+        sudo modprobe mk_arcade_joystick_rpi map=1,2
+        echo "Modifiche effettuate!"
+        ;;
+        2)
+        echo "Disattivo la breakout!"
+        sudo perl -p -i -e 's/mk_arcade_joystick_rpi/#mk_arcade_joystick_rpi/g' /etc/modules
+        sudo perl -p -i -e 's/dtoverlay=audremap,pins_18_19/#dtoverlay=audremap,pins_18_19/g' /boot/config.txt
+        sudo perl -p -i -e 's/dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/#dtoverlay=pwm-2chan,pin=18,func=2,pin2=19,func2=2/g' /boot/config.txt
+        sudo perl -p -i -e 's/disable_audio_dither=1/#disable_audio_dither=1/g' /boot/config.txt
+        sudo modprobe -r mk_arcade_joystick_rpi
+        echo "Modifiche effettuate!"
+        ;;
+esac
 sleep 5
